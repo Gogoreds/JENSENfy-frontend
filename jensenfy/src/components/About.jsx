2d7dbbb88ce0ae5f5a6f4a3399f552aae388e264
Båmi Lee <swedenebomi@gmail.com>
2022-08-29 23:36:24 +0200

    test commit
    Signed-off-by: Båmi Lee <swedenebomi@gmail.com>

--- jensenfy/src/components/About.jsx
+++ jensenfy/src/components/About.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import NavMenu from './navbars/NavMenu'
+import React, { useEffect, useState } from "react";
+import NavMenu from './navbars/NavMenu';
+import sliderImage from "./sliderImage";
+import SliderContent from "./SliderContent";
+import Dots from "./Dots";
+import Arrows from "./Arrows";
 
-function About() {
+
+const len = sliderImage.length - 1;
+
+function About(props) {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
+    <>
     <div>
       <NavMenu />
+      </div>
       <h2>About us</h2>
+      <div className="slider-container">
+      <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
+      <Arrows
+        prevSlide={() =>
+          setActiveIndex(activeIndex < 1 ? len : activeIndex - 1)
+        }
+        nextSlide={() =>
+          setActiveIndex(activeIndex === len ? 0 : activeIndex + 1)
+        }
+      />
     </div>
+    </>
   )
 }
 
---
+++ jensenfy/src/components/sliderImage.jsx
@@ -0,0 +1,41 @@
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+import Third from "../assets/Third.jpg";
+import Janggum from "../assets/Janggum.jpg";
+import Harry from "../assets/Harry.jpg";
+
+
+export default [
+  {
+    title: "Mucic Festival",
+    description: "Our Brand is Jensenfy! Everyday With Jensenfy Music!",
+    urls: First,
+  },
+  {
+    title: "Generation",
+    description: "Values",
+    urls: Second,
+  },
+  {
+    title: "Rock and roll",
+    description: "Why Are Core Values Important?",
+    urls: Third,
+  },
+  {
+    title: "Tradition song Slide",
+    description: " Mission",
+    urls: Janggum,
+  },
+  {
+    title: " R & B",
+    description: "Here are some albums that show an infinite musical spectrum.",
+    urls: Harry,
+  },
+
+  {
+    title: "Ballad ",
+    description: "Create music",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(105).jpg",
+  },
+  
+];
