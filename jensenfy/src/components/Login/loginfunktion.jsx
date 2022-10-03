6011d3efe556ca3a4b37f7495d02d5b297418ae8
Mohammad Arefi <m-arefi@live.com>
2022-10-03 14:57:17 +0200

    withCredentials fix

--- jensenfy/src/components/Login/loginfunktion.jsx
+++ jensenfy/src/components/Login/loginfunktion.jsx
@@ -1,49 +1,37 @@
 import { useNavigate } from "react-router-dom";
-import React from 'react';
-import { useRef, useState, useEffect } from 'react'
-import { useContext } from 'react'
-import AuthContext from './context/AuthProvider';
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { useContext } from "react";
+import AuthContext from "./context/AuthProvider";
 
-import axios from './api/axios';
-const LOGIN_URL = '/auth';
-const REG_URL = '/newUser'
-
-
-
-
+import axios from "./api/axios";
+const LOGIN_URL = "/login";
+const REG_URL = "/newUser";
 
 export default function Login() {
-
-
-
   const [authMode, setAuthMode] = useState("signin");
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin");
-  }
-
-
-
+  };
 
   const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
-
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [errMsg, setErrMsg] = useState('');
+  const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
 
   const [register, setRegister] = useState(false);
 
   useEffect(() => {
     userRef.current.focus();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    setErrMsg('');
-  }, [userName, password])
-
+    setErrMsg("");
+  }, [userName, password]);
 
   const handleRegister = (e) => {
     // prevent the form from refreshing the whole page
@@ -76,103 +64,102 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post(LOGIN_URL,
+      const response = await axios.post(
+        LOGIN_URL,
         JSON.stringify({ userName, password }),
         {
           headers: {
-            'Content-type': 'application/json'
+            "Content-type": "application/json",
           },
-          withCredentials: true,
-
+          withCredentials: false,
         }
       );
       const accessToken = response?.data?.accessToken;
-      setAuth({ userName, password, accessToken })
+      setAuth({ userName, password, accessToken });
       //console.log(JSON.stringify(respone));
-      setUserName('');
-      setPassword('');
+      setUserName("");
+      setPassword("");
       Navigate("/authlog", { replace: true });
-      setSuccess(true)
+      setSuccess(true);
     } catch (err) {
       if (!err?.response) {
-        setErrMsg('No Server Response');
+        setErrMsg("No Server Response");
       } else if (err.response?.status === 400) {
-        setErrMsg('Missing Username or Password');
+        setErrMsg("Missing Username or Password");
       } else if (err.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      }
-      else {
-        setErrMsg('Login Failed');
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
       }
       errRef.current.focus();
     }
-
-  }
-
-
-
-
+  };
 
   if (authMode === "signin") {
     return (
-      <> {success ? (
-        { handleLogin }
-      ) : (
-        <div className="Auth-form-container">
-          <form className="Auth-form" onSubmit={handleLogin}>
-            <div className="Auth-form-content">
+      <>
+        {" "}
+        {success ? (
+          { handleLogin }
+        ) : (
+          <div className="Auth-form-container">
+            <form className="Auth-form" onSubmit={handleLogin}>
+              <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Sign In</h3>
+                <div className="text-center">
+                  Not registered yet?{" "}
+                  <span className="link-primary" onClick={changeAuthMode}>
+                    Sign Up
+                  </span>
+                </div>
+                <div className="form-group mt-3">
+                  <label>Username </label>
+                  <input
+                    className="form-control mt-1"
+                    id="userName"
+                    ref={userRef}
+                    name="userName"
+                    value={userName}
+                    placeholder="username"
+                    onChange={(e) => setUserName(e.target.value)}
+                  />
+                </div>
 
-              <h3 className="Auth-form-title">Sign In</h3>
-              <div className="text-center">
-                Not registered yet?{" "}
-                <span className="link-primary" onClick={changeAuthMode} >
-                  Sign Up
-                </span>
+                <div className="form-group mt-3">
+                  <label>Password</label>
+                  <input
+                    className="form-control mt-1"
+                    name="password"
+                    type="password"
+                    id="password"
+                    value={password}
+                    placeholder="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="d-grid gap-2 mt-3">
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onClick={(e) => handleLogin(e)}
+                  >
+                    Logga in
+                  </button>
+                  <p
+                    ref={errRef}
+                    className={errMsg ? "errmsg" : "offscreen"}
+                    aria-live="assertive"
+                  >
+                    {errMsg}
+                  </p>
+                </div>
               </div>
-              <div className="form-group mt-3">
-                <label>Username </label>
-                <input
-                  className="form-control mt-1"
-                  id='userName'
-                  ref={userRef}
-                  name="userName"
-                  value={userName}
-                  placeholder="username"
-                  onChange={(e) => setUserName(e.target.value)}
-                />
-              </div>
-
-              <div className="form-group mt-3">
-                <label>Password</label>
-                <input
-                  className="form-control mt-1"
-
-                  name="password"
-                  type="password"
-                  id='password'
-                  value={password}
-                  placeholder="password"
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="d-grid gap-2 mt-3">
-                <button
-                  type="submit"
-                  className="btn btn-primary"
-                  onClick={(e) => handleLogin(e)}
-                >
-                  Logga in
-                </button>
-                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-
-
-              </div>
-            </div>
-          </form>
-        </div>
-      )}</>
-    )
+            </form>
+          </div>
+        )}
+      </>
+    );
   }
   // "Register page"
   return (
@@ -190,12 +177,10 @@ export default function Login() {
             <label>Register Your Username here</label>
             <input
               className="form-control mt-1"
-
               name="userName"
               value={userName}
               placeholder="ure username here:"
               onChange={(event) => setUserName(event.target.value)}
-
             />
           </div>
 
@@ -208,22 +193,26 @@ export default function Login() {
               value={password}
               placeholder="ure password here"
               onChange={(event) => setPassword(event.target.value)}
-
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary" onClick={handleRegister}>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleRegister}
+            >
               Registera
             </button>
 
             {/* display success message */}
             {register ? (
-              <p className="text-success">You Are Now Registered Successfully</p>
+              <p className="text-success">
+                You Are Now Registered Successfully
+              </p>
             ) : (
               <p className="text-danger">Please register your account here</p>
             )}
           </div>
-
         </div>
       </form>
     </div>
