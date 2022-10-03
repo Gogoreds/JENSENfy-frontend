a3f36db2be485f7f24047b181f0ec9eb7e2ffb3f
Mo Arefi <m-arefi@live.com>
2022-10-04 00:00:59 +0200

    LeftSideBar in position

--- jensenfy/src/components/Spotify/Components/LeftSideBar.jsx
+++ jensenfy/src/components/Spotify/Components/LeftSideBar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "./LeftSideBarStyles.css";
+import { FaSpotify, FaEllipsisH } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
 
 function LeftSideBar() {
-  return <div>LeftSideBar</div>;
+  return (
+    <div className="leftSideBar">
+      <div className="jensenfyLogo">
+        <i>
+          <FaSpotify />
+        </i>
+        <h3>Jensenfy</h3>
+        <i>
+          <FaEllipsisH />
+        </i>
+      </div>
+      <div className="searchBar">
+        <input type="text" placeholder="What are we listening to?" />
+        <i className="magnifyingglass">
+          <BiSearchAlt />
+        </i>
+      </div>
+    </div>
+  );
 }
 
 export { LeftSideBar };
