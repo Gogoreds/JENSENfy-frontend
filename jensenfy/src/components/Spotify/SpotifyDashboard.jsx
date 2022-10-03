c740c5dcae4677ce623f03cebab39f6bca831367
Mo Arefi <m-arefi@live.com>
2022-10-03 21:44:12 +0200

    SpotifyDash mapping

--- jensenfy/src/components/Spotify/SpotifyDashboard.jsx
+++ jensenfy/src/components/Spotify/SpotifyDashboard.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import NavLoggedIn from '../navbars/NavLoggedIn'
+import React from "react";
+import "./SpotifyDashboardStyles.css";
+// import NavLoggedIn from "../navbars/NavLoggedIn";
+import LeftSideBar from "../LoggedIn/LeftSideBar";
+import RightSideBar from "../LoggedIn/RightSideBar";
+import Dashboard from "../LoggedIn/Dashboard";
 //import useAuth from '../../useAuth'
-
-
-
-
 
 function SpotifyDashboard({ code }) {
   //const accessToken = useAuth(code)
 
-
-
   return (
-
-
-
-    < div >
-      <NavLoggedIn />
+    <div className="App">
+      <div className=""></div>
+      {/* <NavLoggedIn /> */}
+      <LeftSideBar />
+      <Dashboard />
+      <RightSideBar />
       SpotifyDashboard
       {code}
-
-
-
+      <div className="background"></div>
     </div>
-  )
+  );
 }
 
-export default SpotifyDashboard
+export default SpotifyDashboard;
