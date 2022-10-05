853381520cff887e2a28fcdf83699173c0a0b690
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-10-05 11:28:29 +0200

    useNavigate

--- jensenfy/src/components/navbars/NavLoggedIn.jsx
+++ jensenfy/src/components/navbars/NavLoggedIn.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 import LogOutFunction from '../Login/LogOutFunction';
 
 
 function NavLoggedIn() {
 
   const logout = LogOutFunction();
+  const navigate = useNavigate();
+
 
   const signOut = async () => {
     await logout();
+    navigate('/')
   }
 
 
@@ -21,7 +26,7 @@ function NavLoggedIn() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href='/authlog'>My site</Nav.Link>
-            <Nav.Link href='/' onClick={signOut}>Log out</Nav.Link>
+            <Nav.Link onClick={signOut}>Log out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
